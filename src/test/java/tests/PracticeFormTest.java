ee2540d8f2cd6bbca2d9af568d42d172030e9d03
Olga Vasileva <olenka.vasileva.1987@gmail.com>
2025-06-18 11:24:58 +0500

    added PracticeFormTest

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -14,51 +14,57 @@ public class PracticeFormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 5000;
     }
 
     @Test
     void practiceFormTest() {
         open("/automation-practice-form");
 
+        //убираем рекламу
+        executeJavaScript("$('footer').remove();");
+        executeJavaScript("$('#fixedban').remove();");
 
+        // Заполнение формы
         $("#firstName").setValue("Olga");
         $("#lastName").setValue("Vasileva");
-
         $("#userEmail").setValue("olgatest@v.com");
-
         $("label[for='gender-radio-2']").click();
-
         $("#userNumber").setValue("89001114455");
 
+        // Выбор даты рождения
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption("1987");
         $(".react-datepicker__month-select").selectOption("November");
-        $(".react-datepicker__day-006").click();
+        $$(".react-datepicker__day").findBy(text("6")).click();
 
+        // Ввод предмета
         $("#subjectsInput").setValue("Biology").pressEnter();
 
-
+        // Выбор хобби
         $("label[for='hobbies-checkbox-1']").click();
         $("label[for='hobbies-checkbox-2']").click();
 
+        // Загрузка изображения
         $("#uploadPicture").uploadFromClasspath("котик.jpg");
 
+        // Ввод адреса
         $("#currentAddress").setValue("Уфа, Владивостокская 23");
 
+        // Выбор штата и города
         $("#state").click();
         $("#react-select-3-input").setValue("NCR").pressEnter();
-
         $("#city").click();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
 
+        // Подтверждение отправки формы
         $("#submit").click();
 
+        // Проверка результатов в модальном окне
         $(".table-responsive").shouldHave(text("Olga Vasileva"));
         $(".table-responsive").shouldHave(text("olgatest@v.com"));
         $(".table-responsive").shouldHave(text("Female"));
         $(".table-responsive").shouldHave(text("8900111445"));
-        $(".table-responsive").shouldHave(text("06 November,1987"));
+        $(".table-responsive").shouldHave(text("6 November,1987"));
         $(".table-responsive").shouldHave(text("Biology"));
         $(".table-responsive").shouldHave(text("Sports, Reading"));
         $(".table-responsive").shouldHave(text("котик.jpg"));
