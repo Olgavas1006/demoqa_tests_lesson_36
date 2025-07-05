0c121c29d89cc5bc51514b82f46a04821fe90d9d
Olga Vasileva <olenka.vasileva.1987@gmail.com>
2025-07-05 20:29:03 +0500

    update RegistrationWithFakerTests

--- src/test/java/tests/RegistrationWithFakerTests.java
+++ src/test/java/tests/RegistrationWithFakerTests.java
@@ -1,34 +1,12 @@
 package tests;
 
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
-
-import java.util.Locale;
+import static tests.TestData.*;
 
 public class RegistrationWithFakerTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
-
-    Faker faker = new Faker(new Locale("en"));
-    String firstName = faker.name().firstName();
-    String lastName = faker.name().lastName();
-    String userEmail = faker.internet().emailAddress();
-    String gender = faker.options().option("Male", "Female", "Other");
-    String userNumber = faker.phoneNumber().subscriberNumber(10);
-    String day = String.format("%02d", faker.number().numberBetween(1,30));
-    String month = faker.options().option(
-            "January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-    );
-    String year = String.valueOf(faker.number().numberBetween(1900, 2100));
-
-    String subject = faker.options().option("Math", "Biology", "Chemistry", "Physics");
-    String hobby = faker.options().option("Sports", "Reading", "Music");
-    String pictureName = faker.options().option("котик.jpg");
-    String currentAddress = faker.address().fullAddress();
-    String state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
-    String city = getRandomCity(state);
 
     @Test
     void successfulRegistrationTest() {
@@ -60,14 +38,4 @@ public class RegistrationWithFakerTests extends TestBase {
                 .checkResult("State and City", state + " " + city);
     }
 
-    private String getRandomCity(String state) {
-        return switch (state) {
-            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
-            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
-            case "Haryana" -> faker.options().option("Karnal", "Panipat");
-            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
-            default -> throw new IllegalArgumentException("Unknown state: " + state);
-        };
-
-    }
 }
---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,40 @@
+package tests;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class TestData {
+   public static final Faker faker = new Faker(new Locale("en"));
+   public static String firstName = faker.name().firstName();
+   public static String lastName = faker.name().lastName();
+   public static String userEmail = faker.internet().emailAddress();
+   public static String gender = faker.options().option("Male", "Female", "Other");
+   public static String userNumber = faker.phoneNumber().subscriberNumber(10);
+   public static String day = String.format("%02d", faker.number().numberBetween(1, 30));
+   public static String month = faker.options().option(
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+    );
+   public static String year = String.valueOf(faker.number().numberBetween(1900, 2100));
+
+   public static String subject = faker.options().option("Math", "Biology", "Chemistry", "Physics");
+   public static String hobby = faker.options().option("Sports", "Reading", "Music");
+   public static String pictureName = faker.options().option("котик.jpg");
+   public static String currentAddress = faker.address().fullAddress();
+   public static String state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
+   public static String city = getRandomCity(state);
+
+
+    private static String getRandomCity(String state) {
+        return switch (state) {
+            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana" -> faker.options().option("Karnal", "Panipat");
+            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
+            default -> throw new IllegalArgumentException("Unknown state: " + state);
+        };
+
+    }
+
+}
