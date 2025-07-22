e96a9bf47afe0c29fae4d2023d988fa7bc59be3c
Olga Vasileva <olenka.vasileva.1987@gmail.com>
2025-07-22 23:30:49 +0500

    added remote demoqa test

--- src/test/java/tests/PracticeFormWithJenkinsTest.java
+++ src/test/java/tests/PracticeFormWithJenkinsTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,11 @@ public class PracticeFormWithJenkinsTest {
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
     }
 
     @Test
