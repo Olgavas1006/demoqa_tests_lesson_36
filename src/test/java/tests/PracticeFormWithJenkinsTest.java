a782a88ba5261f7b4bc00fc4d418256258543b86
Olga Vasileva <olenka.vasileva.1987@gmail.com>
2025-07-22 09:26:06 +0500

    added remote demoqa test

--- src/test/java/tests/PracticeFormWithJenkinsTest.java
+++ src/test/java/tests/PracticeFormWithJenkinsTest.java
@@ -16,6 +16,7 @@ public class PracticeFormWithJenkinsTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
