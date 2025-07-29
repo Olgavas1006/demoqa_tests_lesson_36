1f8f67cfa484aacc55a85d4e5a0f9f7dd4107705
Olga Vasileva <olenka.vasileva.1987@gmail.com>
2025-07-29 23:45:22 +0500

    update TestBaseWithJenkins

--- src/test/java/tests/TestBaseWithJenkins.java
+++ src/test/java/tests/TestBaseWithJenkins.java
@@ -16,7 +16,7 @@ public class TestBaseWithJenkins {
     @BeforeAll
     static void setUp() {
         Configuration.browser = System.getProperty("browser", "firefox");
-        Configuration.browserVersion = System.getProperty("version", "125");
+        Configuration.browserVersion = System.getProperty("version", "125.0");
         Configuration.browserSize = System.getProperty("windowSize", "1920x1080");
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.pageLoadStrategy = "eager";
